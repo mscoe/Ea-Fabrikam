6bd2525c9a9cfe65221d1841b5aa6521aafbf157
Karthik H Raju <karthik.h.raju@gmail.com>
2015-07-31 12:14:17 +0530

    Added interface projects to Command; and Added post build script to generate nuget packs

---
+++ src/v1/Fabrikam.Module1.Uc1.Command.v1/Fabrikam.Module1.Uc1.Services.Wcf.v1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fabrikam.Module1.Uc1.Services.Wcf.v1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
